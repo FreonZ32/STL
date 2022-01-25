4b2674766155339f0258f23d452967baebe79a18
BuildTools <unconfigured@null.spigotmc.org>
2022-01-25 19:15:14 +0500

    HW

--- GAIBase/Source.cpp
+++ GAIBase/Source.cpp
@@ -5,6 +5,7 @@
 #include<map>
 #include<iterator>
 #include<Windows.h>
+#include<fstream>
 using namespace std;
 using std::cin;
 using std::cout;
@@ -19,6 +20,20 @@ string randData()
 	stringstream yearss; yearss << (2000 + rand() % 22); yearss >> year;
 	data.append("."); data.append(month); data.append("."); data.append(year); data.append(": ");
 	return data;
+}
+
+int HMstr(const string& name) //Return number of strings on file
+{
+	int size = 0;
+	ifstream fin(name);
+	if (fin.is_open())
+	{
+		string str;
+		while (getline(fin, str)) size++;
+		fin.close();
+	}
+	else cerr << "File no found!" << endl;
+	return size;
 }
 
 void fill_map(map <string, list<string>>& base, unsigned short int elements)
@@ -87,6 +102,54 @@ void print_all_map(map <string, list<string>>& base)
 	cout << endl;
 }
 
+void print_to_file(map <string, list<string>>& base)
+{
+	ofstream WriteToFile("File.txt");
+	for (map<string, list<string>>::iterator it = base.begin(); it != base.end(); ++it)
+	{
+		WriteToFile << it->first << endl;
+		for (string i : it->second)WriteToFile << "  " << i << ";" << endl;
+	}
+	WriteToFile.close();
+}
+
+map <string, list<string>> read_from_file(map <string, list<string>>& base)
+{
+	ifstream ReadFromFile("File.txt");
+	int size = HMstr("File.txt");
+	string str, Number, Contravention;
+	if (ReadFromFile.is_open())
+	{
+		for (int i = 0; i < size; i++)
+		{
+			string buffer;
+			getline(ReadFromFile, str, '\n');
+			int charNum = 256 + int(str[0]);
+			if (charNum >= 192 && charNum <= 223) 
+			{
+				Number = str; 
+				buffer = str;
+			}
+			else if(charNum == 288)
+			{
+				charNum = str.size();
+				str.resize(charNum-1);
+				Contravention = str;
+			}
+			if(i&&buffer!=Number)insert_new(base, Number, Contravention);
+		}
+	}
+	ReadFromFile.close();
+	return base;
+}
+
+void clear_file()
+{
+	ofstream ReadTheFile;
+	ReadTheFile.open("File.txt", std::ofstream::out | std::ofstream::trunc);
+	ReadTheFile.close();
+}
+
 void print_info_number(map <string, list<string>>& base, string& number)
 {
 	map<string, list<string>>::iterator it = base.find(number);
@@ -129,12 +192,14 @@ void find_crime_by_smth(map <string, list<string>>& base, string& str, string wh
 }
 
 
-#define GAI
+//#define GAI_CR_AND_FUNC
+//#define WRITE_TO_FILE
+#define READ_FILE
 
 void main()
 {
 	setlocale(LC_ALL,"Russian");
-#ifdef GAI
+#ifdef  GAI_CR_AND_FUNC
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	map <string, list<string>>Base;
@@ -143,24 +208,46 @@ void main()
 	fill_map(Base, number);
 	print_all_map(Base);
 	string num;
-	//cout << "Какой номер хотите распечатать?: "; cin >> num;
-	//print_info_number(Base, num);
-	//cout << "Введите номер: "; cin >> num;
-	//string contravention;
-	//cin.get();	//Сatch Enter after num cin;
-	////cin.ignore();
-	////fflush(stdin);
-	//cout << "Введите правонарушение в формате (дата: Правонарушение): "; getline(cin, contravention);
-	//insert_new(Base, num, contravention);
-	//print_all_map(Base);
+	cout << "Какой номер хотите распечатать?: "; cin >> num;
+	print_info_number(Base, num);
+	cout << "Введите номер: "; cin >> num;
+	string contravention;
+	cin.get();	//Сatch Enter after num cin;
+	//cin.ignore();
+	//fflush(stdin);
+	cout << "Введите правонарушение в формате (дата: Правонарушение): "; getline(cin, contravention);
+	insert_new(Base, num, contravention);
+	print_all_map(Base);
+
 	string smth;
-	cin.get();
+	//cin.get();
 	cout << "Введите дату по которой будет проведен поиск(в формате дд.мм.гггг): "; getline(cin, smth);
 	find_crime_by_smth(Base,smth,"Data");
-	cin.get();
+	//cin.get();
 	cout << "Введите нарушение по которому будет проведен поиск: "; getline(cin, smth);
 	find_crime_by_smth(Base, smth,"Crime");
-#endif // GAI
+#endif //  GAI_CR_AND_FUNC
 
+#ifdef WRITE_TO_FILE
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	map <string, list<string>>Base;
+	int number = 0;
+	cout << "Сколько элементов хотите вставить?: "; cin >> number;
+	fill_map(Base, number);
+	print_all_map(Base);
+	print_to_file(Base);
+	system("start notepad File.txt");
+#endif // WRITE_TO_FILE
+
+
+#ifdef READ_FILE
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	system("start notepad File.txt");
+	map <string, list<string>>Base;
+	read_from_file(Base);
+	print_all_map(Base);
+#endif // READ_FILE
 
 }
