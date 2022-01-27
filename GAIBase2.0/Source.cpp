29ed5ef0a75f9dc89938d3a86ca6210051bccabd
BuildTools <unconfigured@null.spigotmc.org>
2022-01-27 19:23:58 +0500

    HW

--- GAIBase2.0/Source.cpp
+++ GAIBase2.0/Source.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include<string>
+#include<iterator>
 #include<map>
 #include<list>
 #include<string.h>
@@ -11,30 +11,97 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+void ErrorCout(string what)
+{
+	HANDLE hConsoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsoleHandle, 4);
+	cout << "ERROR: Wrong " << what << "!" << endl;
+	SetConsoleTextAttribute(hConsoleHandle, 7);
+}
+
+bool DataCrimrOut(int year, int month, int day)
+{
+	time_t rawtime; struct tm* timeinfo; time(&rawtime); timeinfo = localtime(&rawtime);
+	if (year <= timeinfo->tm_year+1900-20)return false;
+	else return true;
+}
+class TimeData
+{
+	unsigned short int year;
+	unsigned short int month;
+	unsigned short int day;
+public:
+	void set_year(int year)
+	{
+		if(year < 1900||year > 3000)
+		{ErrorCout("year"); this->year = 1900;}
+		this->year = unsigned short int(year);
+	}
+	void set_month(int month)
+	{
+		if (month > 12 || month == 0)
+		{ErrorCout("month"); this->month = 1;}
+		this->month = unsigned short int(month);
+	}
+	void set_day(int day)
+	{
+		if (day > 31 || day == 0)
+		{ErrorCout("day"); this->day = 1;}
+		this->day = unsigned short int(day);
+	}
+	unsigned short int get_year()const { return year; }
+	unsigned short int get_month()const { return month; }
+	unsigned short int get_day()const { return day; }
+
+	TimeData(int year  = 1900,int month = 1,int day = 1)
+	{
+		time_t rawtime;struct tm* timeinfo;time(&rawtime);timeinfo = localtime(&rawtime);
+		set_year(year);
+		set_month(month);
+		set_day(day);
+	}
+	~TimeData(){}
+	TimeData& operator=(const TimeData& other)
+	{
+		if (this == &other)return *this;
+		this->~TimeData();
+		this->year = other.year;
+		this->month = other.month;
+		this->day = other.day;
+		return*this;
+	}
+	void print()const
+	{
+		cout << year << "." << month << "." << day << ":";
+	}
+};
+ostream& operator<<(ostream& os, const TimeData& obj)
+	{
+		return os << obj.get_year() << "."<< obj.get_month()<<"."<<obj.get_day() << ":";
+	}
+
 class NumberInfo
 {
 	bool Active;
-	time_t rawtime;
-	struct tm* timeinfo;
+	TimeData Time;
 	string Crime;
-
 public:
-	NumberInfo(string Crime, int year, int mon, int day, bool Active = true)
+	NumberInfo(string Crime, int year, int month, int day, bool Active = true)
 	{
-		time(&rawtime);
-		this->timeinfo = localtime(&rawtime);
-		this->timeinfo->tm_year = year;
-		this->timeinfo->tm_mon = mon;
-		this->timeinfo->tm_mday = day;
+		this->Active = Active;
+		this->Time = TimeData(year, month, day);
 		this->Crime = Crime;
 	}
 	~NumberInfo(){}
 
-	void print()
+	void print()const
 	{
-		char time[20];
-		strftime(time,20,"%m/%d/%y:", timeinfo);
-		cout << time << " " << Crime << endl;
+		HANDLE hConsoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+		if (Active)
+		{SetConsoleTextAttribute(hConsoleHandle, 2); cout << "  " << Time << "\t" << Crime << endl; }
+		else
+		{SetConsoleTextAttribute(hConsoleHandle, 8); cout << "  " << Time << "\t" << Crime << endl; }
+		SetConsoleTextAttribute(hConsoleHandle, 7);
 	}
 };
 
@@ -56,17 +123,17 @@ void fill_map(map <string, list<NumberInfo>>& base, unsigned short int elements)
 		list<NumberInfo> Crimes;
 		for (int i = 1; i < 9; i++)
 		{
-			int year = 1900 + rand()%120;
+			int year = 1990 + rand()%32;
 			int mon = 1 + rand() % 12; 
 			int day = 1+rand()%30;
 			unsigned short int NumbOfCrime = rand() % 4;
 			switch (NumbOfCrime)
 			{
-				case 1:Crimes.push_back(NumberInfo("Проезд на запрещающий цвет светофора",year, mon, day)); break;
-				case 2:Crimes.push_back(NumberInfo("Пересечение сплошной", year, mon, day)); break;
-				case 3:Crimes.push_back(NumberInfo("Остановка в запрещенном месте", year, mon, day)); break;
-				case 4:Crimes.push_back(NumberInfo("Нарушение правил применения ремней безопасности", year, mon, day)); break;
-				case 5:Crimes.push_back(NumberInfo("Нарушение правил маневрирования", year, mon, day)); break;
+				case 1:Crimes.push_back(NumberInfo("Проезд на запрещающий цвет светофора",year, mon, day,DataCrimrOut(year, mon, day))); break;
+				case 2:Crimes.push_back(NumberInfo("Пересечение сплошной", year, mon, day, DataCrimrOut(year, mon, day))); break;
+				case 3:Crimes.push_back(NumberInfo("Остановка в запрещенном месте", year, mon, day, DataCrimrOut(year, mon, day))); break;
+				case 4:Crimes.push_back(NumberInfo("Нарушение правил применения ремней безопасности", year, mon, day, DataCrimrOut(year, mon, day))); break;
+				case 5:Crimes.push_back(NumberInfo("Нарушение правил маневрирования", year, mon, day, DataCrimrOut(year, mon, day))); break;
 				default:break;
 			}
 		}
