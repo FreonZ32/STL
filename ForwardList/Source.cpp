a3168dbf8ce847a5f05db7bbc04aaf8713f03718
BuildTools <unconfigured@null.spigotmc.org>
2022-01-28 20:29:45 +0500

    HW

--- ForwardList/Source.cpp
+++ ForwardList/Source.cpp
@@ -1,45 +1,93 @@
-public interface ICar
+#include<iostream>
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
+class Car
 {
-    bool EngineIsRunning{ get; }
+    class Engine;
+    class FuelTank;
+    class FuelTankDisplay;
 
-    void EngineStart();
+    class Engine
+    {
+        bool EngineIsRunning;
+    public:
+        bool get_EngineIsRunning()const
+        {return EngineIsRunning;}
+        void set_EngineIsRunning(bool EngineIsRunning)
+        {this->EngineIsRunning = EngineIsRunning;}
 
-    void EngineStop();
+        Engine(bool EngineIsRunning = false)
+        {this->EngineIsRunning = EngineIsRunning; }
+        ~Engine(){}
 
-    void Refuel(double liters);
+        void EngineStart() 
+        {EngineIsRunning = true;}
+        void EngineStop()
+        {EngineIsRunning = false;}
 
-    void RunningIdle();
-}
+        void Consume(FuelTank& FueTan,Engine& Eng)
+        {
+            double fillLevel = FueTan.get_Fill_Level();
+            fillLevel -= 0.0003;
+            FueTan.set_FillLevel(fillLevel,Eng);
+        }
 
-public interface IEngine
-{
-    bool IsRunning{ get; }
+        void RunningIdle();
+    };
+    class FuelTank
+    { 
+        double FillLevel;
+        bool IsOnReserve;
+        bool IsComplete;
+        void Refuel(double liters);
 
-    void Consume(double liters);
+    public:
+        double get_Fill_Level()const
+        {return FillLevel;}
+        void set_FillLevel(double FillLevel, Engine& Eng)
+        {
+            if (FillLevel<=double(5)) { this->IsOnReserve = true; }
+            else if (FillLevel == 0) { Eng.EngineStop(); this->IsComplete = true; }
+            else this->FillLevel = FillLevel;
+        }
+        FuelTank(double FillLevel = 20,bool IsOnReserve = false,bool IsComplete = false)
+        {
+            this->FillLevel = FillLevel;
+            this->IsOnReserve = IsOnReserve;
+            this->IsComplete = IsComplete;
+        }
+        FuelTank(){}
+        class FuelTankDisplay
+        {
+            double FillLevel;
+            bool IsOnReserve;
+            bool IsComplete;
+        };
+       // friend class Engine;
+    };
+    Engine Eng;
+    FuelTank FueTan;
+    FuelTank::FuelTankDisplay FueTanDis;
+public:
+    Engine get_Eng()const
+    {return Eng;}
+    FuelTank get_FueTan()const
+    {return FueTan;}
+    FuelTank::FuelTankDisplay get_FueTanDis()
+    {return FueTanDis;}
 
-    void Start();
+    void CarON()
+    {
+        char C;
+        C = getchar();
+        while (C == 13)
+        {
 
-    void Stop();
-}
+        }
 
-public interface IFuelTank
-{
-    double FillLevel{ get; }
+    }
+};
 
-    bool IsOnReserve{ get; }
-
-    bool IsComplete{ get; }
-
-    void Consume(double liters);
-
-    void Refuel(double liters);
-}
-
-public interface IFuelTankDisplay
-{
-    double FillLevel{ get; }
-
-    bool IsOnReserve{ get; }
-
-    bool IsComplete{ get; }
-}
